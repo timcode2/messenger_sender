887ec7e74b9cf4c78b3a2efad3f3172455a2920d
Artem Matrashov <artem.m2013@gmail.com>
2023-10-11 13:32:04 +0300

    create CRUD-system

--- main/views.py
+++ main/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from main.models import Massage
 
 
+def home_page(request):
+    return render(request, "main/home.html")
 
+
+class MassageListView(ListView):
+    model = Massage
+
+
+class MassageDetailView(DetailView):
+    model = Massage
+
+
+class MassageCreateView(CreateView):
+    model = Massage
+    fields = ('subject', 'subject_body')
+    success_url = reverse_lazy('main:messenger_list')
+
+
+class MassageUpdateView(UpdateView):
+    model = Massage
+    fields = ('subject', 'subject_body')
+    success_url = reverse_lazy('main:messenger_list')
+
+
+class MassageDeleteView(DeleteView):
+    model = Massage
+    success_url = reverse_lazy('main:messenger_list')
