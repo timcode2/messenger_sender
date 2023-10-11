3590ee7b6086180dcc8b56be778e24965f45e2e3
Artem Matrashov <artem.m2013@gmail.com>
2023-10-11 13:31:35 +0300

    create model Massage

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Massage(models.Model):
+    subject = models.CharField(max_length=100, verbose_name='тема')
+    subject_body = models.TextField(verbose_name='содержание')
+
+    def __str__(self):
+        return f'self.subject'
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
